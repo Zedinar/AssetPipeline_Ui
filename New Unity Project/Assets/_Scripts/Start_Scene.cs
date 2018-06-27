985f850d46221364a40047a9215ce3cd648eebec
Zedinar/Raul <zedinar@gmail.com>
2018-06-27 12:32:26 -0600

    Upload, still need to fix pause menu script

---
+++ New Unity Project/Assets/_Scripts/Start_Scene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_Scene: MonoBehaviour {
+
+    public bool GameStart = false;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Time.timeScale = 0.0f;
+    }
+        
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Start_Game()
+        {
+            if (GameStart == false)
+            {
+                Time.timeScale = 1.0f;
+                GameStart = true;
+            }
+        }
+}
---
+++ New Unity Project/Assets/_Scripts/Toggle_Pause_Screen.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toggle_Pause_Screen : MonoBehaviour {
+
+    public GameObject Canvas;
+    private bool Paused = false;
+
+    void Start()
+    {
+        Canvas.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (Paused == true && GameObject.Find("Start_Game").GetComponent<Start_Scene>().GameStart))
+            {
+                Time.timeScale = 1.0f;
+                Canvas.gameObject.SetActive(false);
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                Paused = false;
+            }
+            else
+            {
+                Time.timeScale = 0.0f;
+                Canvas.gameObject.SetActive(true);
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                Paused = true;
+            }
+        }
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1.0f;
+        Canvas.gameObject.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+}
